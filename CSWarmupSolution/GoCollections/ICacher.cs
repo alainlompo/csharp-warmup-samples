9fd041ccd20083747b3fabbf06e03781aba3d292
alainlompo <alain.lompo@gmail.com>
2015-11-02 23:06:08 +0100

    Implementing a simple caching system using dictionaries

---
+++ CSWarmupSolution/GoCollections/ICacher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoCollections
+{
+    interface ICacher<T, E>
+    {
+        T GetElement(E idObject);
+
+        /// <summary>
+        /// Load the record
+        /// </summary>
+        /// <param name="idObject"></param>
+        /// <returns></returns>
+        T LoadRecord(E idObject);
+
+        /// <summary>
+        /// Checks if the record is stale or not
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        bool IsStale(T result);
+
+        /// <summary>
+        /// Implements cache removal policy here
+        /// </summary>
+        void discardEntries();
+
+    }
+}
