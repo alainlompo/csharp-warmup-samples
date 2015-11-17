1557452578b2dc3c7b1480a3b540a597c0ea4c89
alainlompo <alain.lompo@gmail.com>
2015-11-17 01:21:43 +0100

    Developping A Console App Service Host for Wcf services

---
+++ CSWarmupSolution/WcfSimpleMessagingClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using GoWCF;
+
+namespace WcfSimpleMessagingServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost messagingHost = new ServiceHost(typeof(GoWCF.MessagingService)))
+            {
+                messagingHost.Open();
+                Console.WriteLine("Messaging service initialized...");
+                Console.WriteLine("Type quit to close the service and exit....");
+                String text = null;
+                while (true)
+                {
+                    text = Console.ReadLine();
+                    if (text.Trim().ToUpper().Equals("QUIT"))
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Terminating the messaging service host....");
+                messagingHost.Close(TimeSpan.FromSeconds(5.00D));
+            }
+        }
+    }
+}
