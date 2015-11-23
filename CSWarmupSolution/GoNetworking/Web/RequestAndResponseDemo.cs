daf7c20e96b4e4b538f2262977651fd7b43967de
alainlompo <alain.lompo@gmail.com>
2015-11-23 22:28:15 +0100

    Illsutrating the use of HttpWebRequest and HttpWebResponse objects

---
+++ CSWarmupSolution/GoNetworking/Web/RequestAndResponseDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoNetworking.Web
+{
+    class RequestAndResponseDemo
+    {
+        public static void DoRequestResponseDemo()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://github.com/alainlompo");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream responseStream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(responseStream))
+            {
+                string pageDatas = reader.ReadToEnd();
+                Console.WriteLine(pageDatas);
+                Console.ReadKey();
+            }
+        }
+    }
+}
