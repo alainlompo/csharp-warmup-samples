0bccc21b4bbe1daba979d5db70b840551ba7f7a7
alainlompo <alain.lompo@gmail.com>
2016-02-01 03:27:06 +0100

    Programmatic access to github API using octokit .net client library

---
+++ CSWarmupSolution/GoGithub/SimpleApiDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GoGithub
+{
+    public class SimpleApiDemo
+    {
+
+        public async void DoGitDemo()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("MyApps"));
+            var user = await github.User.Get("jlhg");
+            Console.WriteLine(user.Followers + " folks are interested in jlhg's space");
+        }
+
+       
+    }
+}
---
+++ CSWarmupSolution/GoGithubConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoGithubConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoGithub.SimpleApiDemo sAD = new GoGithub.SimpleApiDemo();
+            sAD.DoGitDemo();
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
