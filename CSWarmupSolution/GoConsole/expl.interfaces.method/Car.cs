1d92f3fd327e81f9a6b35e3318b82af76363f6f4
alainlompo <alain.lompo@gmail.com>
2016-03-17 03:35:36 +0100

    Illustrating explicit interface method implementation (EIMI) in C#

---
+++ CSWarmupSolution/GoConsole/expl.interfaces.method/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoConsole.expl.interfaces.method
+{
+    class Car:IDrivable
+    {
+        /// <summary>
+        /// This methods signature matches that of the IDrivable interface
+        /// so the C# considers it as the interface's drive method implementation
+        /// </summary>
+        /// <param name="speed"></param>
+ 
+        public void drive(int speed)
+        {
+            Console.WriteLine("This car is going at the speed of " + speed + " km/h");
+
+        }
+
+
+    }
+}
---
+++ CSWarmupSolution/GoConsole/expl.interfaces.method/MotorByke.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoConsole.expl.interfaces.method
+{
+    class MotorByke:IDrivable
+    {
+
+        /// <summary>
+        /// This is an EIMI or explicit interface method implementation
+        /// for the IDrivable interface. A simple test will show that it is the method called
+        /// when accessing the drive method from an IDrivable reference cast from a MotorByke reference
+        /// </summary>
+        /// <param name="speed"></param>
+        public void IDrivable.drive(int speed)
+        {
+            Console.WriteLine("This is an explicit interface method implemention of the drive method from IDrivable");
+        }
+
+        /// <summary>
+        /// This method is no longer considered the implementation for the interface drive method since
+        /// there is an EIMI for that 
+        /// </summary>
+        /// <see cref=">">the drive method above</see>
+        /// <param name="speed"></param>
+        public void drive(int speed)
+        {
+            Console.WriteLine("Driving at the speed of " + speed + " km/h");
+        }
+    }
+}
